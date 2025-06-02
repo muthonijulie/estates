e22f2056262c07c7fd90777e5fd15f1601d571db
muthonijulie <julius.muthoni22@students.dkut.ac.ke>
2025-06-02 21:29:52 +0300

    updated contact

--- backend/models/Contact.js
+++ backend/models/Contact.js
@@ -5,9 +5,11 @@ const contactSchema = new mongoose.Schema(
         name:{ type: String, required: true },
         email: { type: String, required: true },
         subject: { type: String, required: true },
-        message: String (required),
-        createdAt: Date,
-        updatedAt: Date
+        message: { type: String, required: true },
+        createdAt: {
+            type: Date
+        },
+        updatedAt: {type:Date}
       }
 
 )
