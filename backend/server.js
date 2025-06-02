a04db362c828ac7285e3da0dc37440f62063eb27
muthonijulie <julius.muthoni22@students.dkut.ac.ke>
2025-06-02 15:16:10 +0300

    Server.js file

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,19 @@ const express=require('express');
 const app=express();
 require("dotenv").config();
 const cors=require('cors');
+const mongoose=require('mongoose');
+//connecting to the database
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log("Connected to MongoDB");
+}).catch((err) => {
+    console.error("Error connecting to MongoDB:", err);
+});
+//middleware
+app.use(express.json());
+//importing routes
 
 
 
